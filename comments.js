e7616da08f8e6933a0bb9872e4ca6e630812ea6b
javierledesma24 <72327372+javierledesma24@users.noreply.github.com>
2023-11-27 20:54:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
+// Create a web server 
 
 const fs = require('fs');
 const http = require('http');
