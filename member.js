22036791204854c95948f9156edcbdd84489220f
javierledesma24 <72327372+javierledesma24@users.noreply.github.com>
2023-11-27 20:44:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var memberList = document.getElementById("member-list");
+    var skillList = document.getElementById("skill-list");
+    var projectList = document.getElementById("project-list");
+    member.style.color = "#fff";
+    skill.style.color = "#fff";
+    project.style.color = "#fff";
+    memberList.style.display = "block";
+    skillList.style.display = "none";
+    projectList.style.display = "none";
+}
